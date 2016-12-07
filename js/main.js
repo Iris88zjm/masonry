7eb2199c400ea9c825e5246576484cf3af0831cf
Iris88zjm <327055610@qq.com>
2016-12-07 10:02:54 +0800

    merge #1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 (function($){
   $(function(){
     var sources = [
-      { title: 'targeting ', img: 'images/1.jpg', content: '1nspired by hashtags and store locations.', name: 'Jack1', id: 1, },
-      { title: 'influences ', img: 'images/2.jpg', content: '2nspired by hashtags and store locations.', name: 'Jack2', id: 2, },
-      { title: 'products', img: 'images/3.jpg', content: '3nspired by hashtags and store locations.', name: 'Jack3', id: 3, },
-      { title: 'influences ', img: 'images/4.jpg', content: '4nspired by hashtags and store locations.', name: 'Jack4', id: 4, },
-      { title: 'connected ', img: 'images/8.jpg', content: '8nspired by hashtags and store locations.', name: 'Jack5', id: 5, },
-      { title: 'irrelevants', img: 'images/5.jpg', content: '5nspired by hashtags and store locations.', name: 'Jack6', id: 6, },
-      { title: 'irrelevants', img: 'images/3.jpg', content: '3nspired by hashtags and store locations.', name: 'Jack7', id: 7, },
-      { title: 'products', img: 'images/1.jpg', content: '1nspired by hashtags and store locations.', name: 'Jack8', id: 8, },
-      { title: 'machine ', img: 'images/7.jpg', content: '7nspired by hashtags and store locations.', name: 'Jack9', id: 9, },
-      { title: 'connected ', img: 'images/4.jpg', content: '4nspired by hashtags and store locations.', name: 'Jack10', id: 10, },
-      { title: 'machine ', img: 'images/8.jpg', content: '8nspired by hashtags and store locations.', name: 'Jack11', id: 11, },
-      { title: 'irrelevants', img: 'images/6.jpg', content: '6nspired by hashtags and store locations.', name: 'Jack12', id: 12, },
-      { title: 'machine ', img: 'images/7.jpg', content: '7nspired by hashtags and store locations.', name: 'Jack9', id: 13, },
-      { title: 'irrelevants', img: 'images/5.jpg', content: '5nspired by hashtags and store locations.', name: 'Jack6', id: 14, },
-      { title: 'irrelevants', img: 'images/6.jpg', content: '6nspired by hashtags and store locations.', name: 'Jack12', id: 15, },
+      { id: 1, name: '@fashionambit', thumbnail: 'images/thumbnail.jpg', img: 'images/1.jpg', relevant: 'images/relevant1.jpg', message: 'fashionambit nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 2, name: '@karivdheide', thumbnail: 'images/thumbnail.jpg', img: 'images/2.jpg', relevant: 'images/relevant2.jpg', message: 'karivdheide nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 3, name: '@taramichellebrose', thumbnail: 'images/thumbnail.jpg', img: 'images/3.jpg', relevant: 'images/relevant1.jpg', message: 'karivdheide nspired by hashtags and store locations.', width: '235px', height: '235px',},
+      { id: 4, name: '@1022mari22', thumbnail: 'images/thumbnail.jpg', img: 'images/4.jpg', relevant: 'images/relevant2.jpg', message: 'karivdheide nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 5, name: '@fashionambit', thumbnail: 'images/thumbnail.jpg', img: 'images/5.jpg', relevant: 'images/relevant1.jpg', message: 'fashionambit nspired by hashtags and store locations.',  width: '235px', height: '235px', },
+      { id: 6, name: '@taminajay', thumbnail: 'images/thumbnail.jpg', img: 'images/6.jpg', relevant: 'images/relevant2.jpg', message: 'taminajay nspired by hashtags and store locations.', width: '235px', height: '235px',},
+      { id: 7, name: '@juliesdresscode', thumbnail: 'images/thumbnail.jpg', img: 'images/7.jpg', relevant: 'images/relevant1.jpg', message: 'juliesdresscode nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 8, name: '@dani_nanaa', thumbnail: 'images/thumbnail.jpg', img: 'images/8.jpg', relevant: 'images/relevant2.jpg', message: 'dani_nanaa nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 9, name: '@cristinagheiceanu', thumbnail: 'images/thumbnail.jpg', img: 'images/9.jpg', relevant: 'images/relevant1.jpg', message: 'cristinagheiceanu nspired by hashtags and store locations.',  width: '475px', height: '475px', },
+      { id: 10, name: '@charissa_goldapple', thumbnail: 'images/thumbnail.jpg', img: 'images/10.jpg', relevant: 'images/relevant2.jpg', message: 'charissa_goldapple nspired by hashtags and store locations.',  width: '235px', height: '235px', },
+      { id: 11, name: '@herzundbild', thumbnail: 'images/thumbnail.jpg', img: 'images/11.jpg', relevant: 'images/relevant1.jpg', message: 'herzundbild nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 12, name: '@wietskeee91', thumbnail: 'images/thumbnail.jpg', img: 'images/12.jpg', relevant: 'images/relevant2.jpg', message: 'wietskeee91 nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 13, name: '@felicefleur', thumbnail: 'images/thumbnail.jpg', img: 'images/13.jpg', relevant: 'images/relevant1.jpg', message: 'felicefleur nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 14, name: '@_fashionsabs', thumbnail: 'images/thumbnail.jpg', img: 'images/14.jpg', relevant: 'images/relevant2.jpg', message: '_fashionsabs nspired by hashtags and store locations.',  width: '235px', height: '235px', },
+      { id: 15, name: '@herz_aller_liebst', thumbnail: 'images/thumbnail.jpg', img: 'images/15.jpg', relevant: 'images/relevant1.jpg', message: 'herz_aller_liebst nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 7, name: '@juliesdresscode', thumbnail: 'images/thumbnail.jpg', img: 'images/7.jpg', relevant: 'images/relevant1.jpg', message: 'juliesdresscode nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 8, name: '@dani_nanaa', thumbnail: 'images/thumbnail.jpg', img: 'images/8.jpg', relevant: 'images/relevant2.jpg', message: 'dani_nanaa nspired by hashtags and store locations.', width: '235px', height: '235px',},
+      { id: 9, name: '@cristinagheiceanu', thumbnail: 'images/thumbnail.jpg', img: 'images/9.jpg', relevant: 'images/relevant1.jpg', message: 'cristinagheiceanu nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 10, name: '@charissa_goldapple', thumbnail: 'images/thumbnail.jpg', img: 'images/10.jpg', relevant: 'images/relevant2.jpg', message: 'charissa_goldapple nspired by hashtags and store locations.', width: '235px', height: '235px', },
+      { id: 11, name: '@herzundbild', thumbnail: 'images/thumbnail.jpg', img: 'images/11.jpg', relevant: 'images/relevant1.jpg', message: 'herzundbild nspired by hashtags and store locations.', width: '235px', height: '235px', },
     ];
     _.each(sources, function(s) {
-      $('.grid').append('<div class="grid-item" data-id="'+ s.id +'"><img src="'+ s.img +'"></div>');
+      $('.grid').append('<div class="grid-item" data-id="'+ s.id +'"><img src="'+ s.img +'" width="' + s.width + '"height="' + s.height + '"></div>');
     });
     $('.grid').masonry({
       itemSelector: '.grid-item',
@@ -31,9 +36,11 @@
       _.each(sources, function(s) {
         if(id == s.id){
           $('.info').append(
-              detail({ title: s.title,
+              detail({
                     img: s.img,
-                    content: s.content,
+                    thumbnail: s.thumbnail,
+                    relevant: s.relevant,
+                    message: s.message,
                     name: s.name
               })
           );
@@ -44,6 +51,7 @@
         }
       });
     });
+
     $('.info').on('click', '.btn-close', function() {
       $('.info').removeClass('active');
       $('.detail').remove();
